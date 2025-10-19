532d86c34f7f892e0ba949ff20f2be037a2bafb8
Yeoms <104765570+sujeong-16@users.noreply.github.com>
2025-10-19 22:58:38 +0900

    Delete src/lib directory
    들어가면 안되는 파일이 들어감..

--- src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://efwszctryswwdtcalych.supabase.co'
-const supabaseAnonKey = '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'
-
-// Supabase 프로젝트 대시보드 > Settings > API 에서 두 값 확인 가능!
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
